17fbecf5900252fb5611212964401b17daa014a0
gangaramanavar <gangaramanavar@gmail.com>
2023-04-20 14:42:37 +0530

    added delete

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 import requests
 
 app = Flask(__name__)
+BASE_URL = 'https://reqres.in/api/users'
+array_of_delete_users = []
 
 @app.route('/users', methods=["GET"])
 def get_users():
-    url = 'https://reqres.in/api/users'
-    response = requests.get(url)
-    return jsonify(response.json())
+    all_users = []
+    for page_num in range(1, 4): 
+        response = requests.get(BASE_URL + "?page=" + str(page_num))
+        data = response.json().get('data', [])
+        all_users.extend(data)
+    return render_template('index.html',all_users = all_users, array_of_delete_users = array_of_delete_users)
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    url = 'https://reqres.in/api/users'
-    data = request.json
-    response = requests.post(url, json=data)
-    return jsonify(response.json())
+    data = {
+        "name": request.form["name"],
+        "job": request.form["job"],
+    }
+    response = requests.post(BASE_URL, json=data)
+    return render_template('create_user.html', response=response.json())
 
 @app.route('/users/<userId>', methods=['PUT'])
 def update_user(userId):
-    url = f'https://reqres.in/api/users/{userId}'
-    data = request.json
+    data = request.get_json()
+    url = f'{BASE_URL}/{userId}'
     response = requests.put(url, json=data)
     return jsonify(response.json())
 
-@app.route('/users/<userId>', methods=['DELETE'])
+@app.route('/users/<int:userId>', methods=['GET'])
 def delete_user(userId):
-    url = f'https://reqres.in/api/users/{userId}'
-    response = requests.delete(url)
-    return jsonify(response.json())
-
+    url = f'{BASE_URL}/{userId}'
+    requests.delete(url)
+    array_of_delete_users.append(userId)
+    return redirect(url_for('get_users'))  
 
 if __name__ == '__main__':
     app.run(debug=True)
